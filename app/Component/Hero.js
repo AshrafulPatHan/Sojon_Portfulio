d3d5af6b24760acdb36fe6543b38c230ca142d37
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-01-10 16:50:58 +0600

    updat download

--- app/Component/Hero.js
+++ app/Component/Hero.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { FaDownload } from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DownloadI from '@/public/watsap.jpg'
 
 const Hero = () => {
     return (
@@ -12,16 +13,16 @@ const Hero = () => {
                 <div className='flex flex-col items-start gap-3'>
                     <h2 className='text-2xl sm:text-4xl font-bold'>Hello, I‘m Shojon Houladar</h2>
                     <p className='text-lg sm:text-xl font-medium'>I‘am the student of Govt. Tolaram College </p>
-                    <button onClick={()=>{toast("hello");}}
+                    <a href='watsap.jpg' download='watsap.jpg'
                     className="text-xl box-border border-4 border-blue-400 w-60 h-14 
-                        bg-sky-600 text-white relative group">
-                            <span className="pr-8">Download Resume</span>
+                        bg-sky-600 text-white relative group text-center">
+                            <span className="pr-8  ">Download Resume</span>
                             <span className="bg-sky-900 absolute right-0 top-0  h-full flex 
                             items-center justify-center px-1 group-hover:duration-300 
                             group-hover:w-full w-10 duration-300">
                                 <FaDownload />
                             </span>
-                    </button>
+                    </a>
                 </div>
                 <div >
                     <Image src="https://i.ibb.co.com/pjVtnBj/Vindicator-JE3.webp" 
